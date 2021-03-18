34528f5bebeb2826480c84dcc0f51d2d07e66325
Petr Korolev <sky4winder@gmail.com>
2021-03-18 14:32:05 +0700

    update config

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -2,10 +2,16 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    gancli: {
+      host: "127.0.0.1",
       port: 8545,
       network_id: "*",
     }
   },
+  
   compilers: {
     solc: {
       version: '0.4.23'
