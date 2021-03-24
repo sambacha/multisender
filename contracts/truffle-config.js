678895ac49ecba8f4478f5cfbefd6c425e8bbcf0
idealatom <idealatom@gmail.com>
2021-03-24 11:31:15 +0700

    use ganache-cli as default development network

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   networks: {
-    development: {
+    ganui: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
     },
-    gancli: {
+    development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
     }
   },
-  
+
   compilers: {
     solc: {
       version: '0.4.23'
