ed0c050f761a03c6d9e161435ea962769e9cefec
Petr Korolev <sky4winder@gmail.com>
2021-03-24 12:07:40 +0700

    deploy token update + add config for ropsten

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,3 +1,22 @@
+require('dotenv').config();
+require('babel-register');
+require('babel-polyfill');
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
+    new HDWalletProvider(mnemonic, rpcEndpoint);
+
+const infuraProvider = network => providerWithMnemonic(
+    process.env.MNEMONIC || '',
+    `https://${network}.infura.io/${process.env.INFURA_API_KEY}`
+);
+
+const ropstenProvider = process.env.SOLIDITY_COVERAGE
+    ? undefined
+    : infuraProvider('ropsten');
+
+
 module.exports = {
   networks: {
     ganui: {
@@ -9,7 +28,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
-    }
+    },
+    ropsten: {
+      provider: ropstenProvider,
+      network_id: 3, // eslint-disable-line camelcase
+    },
+
   },
 
   compilers: {
