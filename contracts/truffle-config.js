c9cd31d7e7ee9744f1a8c29e2bd30912f0ab2968
idealatom <idealatom@gmail.com>
2021-03-24 13:26:04 +0700

    change truffle-hdwallet-provider package

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,19 +1,6 @@
 require('dotenv').config();
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
-    new HDWalletProvider(mnemonic, rpcEndpoint);
-
-const infuraProvider = network => providerWithMnemonic(
-    process.env.MNEMONIC || '',
-    `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
-);
-
-const ropstenProvider = process.env.SOLIDITY_COVERAGE
-    ? undefined
-    : infuraProvider('ropsten');
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
@@ -28,8 +15,12 @@ module.exports = {
       network_id: "*",
     },
     ropsten: {
-      provider: ropstenProvider,
-      network_id: 3, // eslint-disable-line camelcase
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 3,       // eslint-disable-line camelcase
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
   },
