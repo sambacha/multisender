d2b8c33038ef41f63ae8d1b76db6a2a94e872f2c
idealatom <idealatom@gmail.com>
2021-03-29 15:56:54 +0700

    initialize contract during migration

--- contracts/migrations/2_deploy_contract.js
+++ contracts/migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
 const UpgradebleStormSender = artifacts.require("UpgradebleStormSender");
 
-module.exports = function (deployer) {
-    deployer.deploy(UpgradebleStormSender);
+module.exports = async function (deployer, network, accounts) {
+    deployer.deploy(UpgradebleStormSender).then((instance) => {
+        instance.initialize(accounts[0]);
+    });
 };
