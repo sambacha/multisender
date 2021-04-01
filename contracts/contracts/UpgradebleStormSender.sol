42de0929de2a5bd587f625fa8d34009c0e68426d
sam <sam@freighttrust.com>
2021-03-31 21:48:30 -0700

    flatten
    Signed-off-by: sam <sam@freighttrust.com>

---
+++ contracts/contracts/UpgradebleStormSender.sol
@@ -0,0 +1,114 @@
+// Roman Storm Multi Sender
+// To Use this Dapp: https://rstormsf.github.io/multisender
+pragma solidity 0.4.26;
+
+import "./OwnedUpgradeabilityStorage.sol";
+import "./Claimable.sol";
+import "./SafeMath.sol";
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+
+  function balanceOf(address who) public view returns (uint256);
+
+  function transfer(address to, uint256 value) public returns (bool);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender)
+    public
+    view
+    returns (uint256);
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  ) public returns (bool);
+
+  function approve(address spender, uint256 value) public returns (bool);
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract UpgradebleStormSender is OwnedUpgradeabilityStorage, Claimable {
+  using SafeMath for uint256;
+
+  event Multisended(uint256 total, address tokenAddress);
+  event ClaimedTokens(address token, address owner, uint256 balance);
+
+  function() public payable {}
+
+  function initialize(address _owner) public {
+    require(!initialized());
+    setOwner(_owner);
+    setArrayLimit(200);
+    boolStorage[keccak256("rs_multisender_initialized")] = true;
+  }
+
+  function initialized() public view returns (bool) {
+    return boolStorage[keccak256("rs_multisender_initialized")];
+  }
+
+  function arrayLimit() public view returns (uint256) {
+    return uintStorage[keccak256("arrayLimit")];
+  }
+
+  function setArrayLimit(uint256 _newLimit) public onlyOwner {
+    require(_newLimit != 0);
+    uintStorage[keccak256("arrayLimit")] = _newLimit;
+  }
+
+  function multisendToken(
+    address token,
+    address[] _contributors,
+    uint256[] _balances
+  ) public payable {
+    if (token == 0x000000000000000000000000000000000000bEEF) {
+      multisendEther(_contributors, _balances);
+    } else {
+      uint256 total = 0;
+      require(_contributors.length <= arrayLimit());
+      ERC20 erc20token = ERC20(token);
+      uint8 i = 0;
+      for (i; i < _contributors.length; i++) {
+        erc20token.transferFrom(msg.sender, _contributors[i], _balances[i]);
+        total += _balances[i];
+      }
+      emit Multisended(total, token);
+    }
+  }
+
+  function multisendEther(address[] _contributors, uint256[] _balances)
+    public
+    payable
+  {
+    uint256 total = msg.value;
+    require(_contributors.length <= arrayLimit());
+    uint256 i = 0;
+    for (i; i < _contributors.length; i++) {
+      require(total >= _balances[i]);
+      total = total.sub(_balances[i]);
+      _contributors[i].transfer(_balances[i]);
+    }
+    emit Multisended(msg.value, 0x000000000000000000000000000000000000bEEF);
+  }
+
+  function claimTokens(address _token) public onlyOwner {
+    if (_token == 0x0) {
+      owner().transfer(this.balance);
+      return;
+    }
+    ERC20 erc20token = ERC20(_token);
+    uint256 balance = erc20token.balanceOf(this);
+    erc20token.transfer(owner(), balance);
+    emit ClaimedTokens(_token, owner(), balance);
+  }
+}
